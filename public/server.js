ada1db2e559285d0ebfc44d1c0952151b3caaf08
Danielggz <dggz1993@gmail.com>
2025-12-09 18:32:25 +0000

    Added functionalities for session and different content depending on role

--- public/server.js
+++ public/server.js
@@ -1,15 +1,18 @@
 var express = require('express');
 const sqlite3 = require('sqlite3');
+//Body parser to use in post requests
 const bodyParser = require('body-parser');
+//Use sqlite3 database
 const db = new sqlite3.Database('database/taskManager.db');
+//Use path for redirecting
 const path = require('path');
 const port = 3000;
+//Use session control
+const session = require('express-session');
 const app = express();
 
 // Serve static files on public folder
 app.use(express.static('../public'));
-
-// Create jsonparser to use in post requests
 
 var jsonParser = bodyParser.json();
 // create application/x-www-form-urlencoded parser
@@ -17,12 +20,20 @@ var urlencodedParser = bodyParser.urlencoded({ extended: true });
 app.use(jsonParser);
 app.use(urlencodedParser);
 
+//Start session
+app.use(session({
+  secret: 'guest',   // change this to something secure
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false }    // set secure: true if using HTTPS
+}));
+
 //LOGIN - validate user details
 app.post('/login', (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
 
-    db.get("SELECT id from users WHERE email='" + email + "' AND password='" + password + "'", 
+    db.get("SELECT id, username, role from users WHERE email='" + email + "' AND password='" + password + "'", 
       function(err, user){
         if(err){
           return res.status(500).json({ error: err.message });
@@ -30,6 +41,8 @@ app.post('/login', (req, res) => {
         if (!user) {
           return res.status(404).json({ message: 'User not found' });
         }else{
+          //Start new session
+          req.session.user = { userId: user.id, username: user.username, role: user.role };
           res.status(201).json(user);
         }
       }
@@ -38,12 +51,25 @@ app.post('/login', (req, res) => {
 
 // CREATE - Add a new task
 app.post('/tasks', (req, res) => {
-  const { title, description } = req.body;
+
+  //Get id from session
+  var idUser = req.session.user.userId;
+  var name = req.body.name;
+  var description = req.body.description;
+  var priority = req.body.priority;
+  var completed = req.body.completed;
+
+  var stmt;
+  stmt = "INSERT INTO tasks (iduser, name, description, priority, completed) VALUES (" + idUser + ", '" + name + "', '" + description + "', " + priority + ", " + completed + ")";
+
+  console.log(stmt);
   db.run(
-    `INSERT INTO tasks (name, description, completed) VALUES (, , )`,
+    stmt,
     function (err) {
-      if (err) return res.status(500).json({ error: err.message });
-      res.status(201).json({ id: this.lastID, title, description, completed: 0 });
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      res.status(201).json({ id: this.lastID, idUser, name, description, priority, completed });
     }
   );
 });
@@ -67,14 +93,27 @@ app.get('/tasks/:id', (req, res) => {
 
 // UPDATE - Modify a task
 app.put('/tasks/:id', (req, res) => {
-  const { title, description, completed } = req.body;
+
+  //Get id from session
+  var idTask = req.body.id;
+  var name = req.body.name;
+  var description = req.body.description;
+  var priority = req.body.priority;
+  var completed = req.body.completed;
+
+  var stmt = "UPDATE tasks SET name = " + name + ", description = " + description + ", priority = " + priority + ",completed = " + completed + " WHERE id = " + idTask;
+
+  console.log(stmt);
   db.run(
-    `UPDATE tasks SET title = ?, description = ?, completed = ? WHERE id = ?`,
-    [title, description, completed ? 1 : 0, req.params.id],
+    stmt,
     function (err) {
-      if (err) return res.status(500).json({ error: err.message });
-      if (this.changes === 0) return res.status(404).json({ message: 'Task not found' });
-      res.json({ id: req.params.id, title, description, completed });
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      if (this.changes === 0) {
+        return res.status(404).json({ message: 'Task not found' });
+      }
+      res.status(201).json({ id: this.lastID, name, description, priority, completed });
     }
   );
 });
@@ -82,8 +121,12 @@ app.put('/tasks/:id', (req, res) => {
 // DELETE - Remove a task
 app.delete('/tasks/:id', (req, res) => {
   db.run(`DELETE FROM tasks WHERE id = ?`, [req.params.id], function (err) {
-    if (err) return res.status(500).json({ error: err.message });
-    if (this.changes === 0) return res.status(404).json({ message: 'Task not found' });
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    if (this.changes === 0) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
     res.json({ message: 'Task deleted successfully' });
   });
 });
@@ -94,6 +137,11 @@ app.get('/', (req, res) => {
 
 app.get('/index', (req, res) => {
   res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
+//Get current session
+app.get('/getSession', (req, res) => {
+  res.json({userId: req.session.user.userId, username: req.session.user.username, role: req.session.user.role});
 });
 
 app.listen(process.env.PORT || 3000,function(req,res){
--- public/views/js/index.js
+++ public/views/js/index.js
@@ -1,3 +1,103 @@
+var userId;
+var username;
+var role;
+
 $(document).ready(function(){
-    
+    //get current session
+    $.ajax({
+        url : 'http://localhost:3000/getSession',
+        type : 'GET',
+        dataType:'json',
+        success : function(data) {   
+            username = data.username;
+            role = data.role;
+            userId = data.userId;
+            $("#currentUser").html("Hello, " + data.username + "  <button class='btn btn-secondary'>Log off</button>");
+
+            //Append create button depending on role
+            if(role == "Admin" || role == "User"){
+                $("#divTasks").append("<button id='btnNewTask' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#modalNewTask'> New Task </button>")
+            }
+
+            //Get current tasks
+            tasks = getTasks();
+        },
+        error : function(request,error)
+        {
+            console.log(error);
+        }
+    });
+
+    $("#formNewTask").submit(function(e){
+        e.preventDefault();
+
+        var name = $("#formName").val();
+        var description = $("#formDescription").val();
+        var priority = $("#formPriority").val();
+        var completed = 0;
+        if($("#formCompleted").prop("checked")){
+            completed = 1;
+        }
+
+        $.ajax({
+            url : 'http://localhost:3000/tasks',
+            type : 'POST',
+            data : {
+                'name' : name,
+                'description' : description,
+                'priority' : priority,
+                'completed' : completed
+            },
+            dataType:'json',
+            success : function(data) {   
+                console.log(data);
+            },
+            error : function(request,error)
+            {
+                $("#errorMsg").html("<div class='alert alert-danger'>Error </div>")
+            }
+        });
+    });
 });
+
+function getTasks(){
+    $.ajax({
+        url : 'http://localhost:3000/tasks',
+        type : 'GET',
+        dataType:'json',
+        success : function(tasks) {   
+            tasks.forEach(task => {
+                var taskDiv = $("<div style='margin-top:15px;'>").addClass("task");
+
+                taskDiv.append(`<hr/><span><strong>Name:</strong> ${task.name}</span>`);
+                taskDiv.append(`<br/><br/><span><strong>Description:</strong> ${task.description}</span>`);
+                taskDiv.append(`<br/><br/><span><strong>Priority:</strong> ${task.priority}</span>`);
+                taskDiv.append(`<br/><br/><span><strong>Completed:</strong> ${task.completed ? "Yes" : "No"}</span>`);
+
+                //Check user role to allow edit and deletion
+                console.log(role + "  " + task.iduser + "  " + userId);
+                if(role == "Admin"){
+                    taskDiv.append("<br/><br/><button class='btnEdit btn btn-success'> Edit </button>");
+                    taskDiv.append("  <button class='btnDelete btn btn-danger'> Delete </button>");
+                }else if(role == "User" && userId == task.iduser){
+                    taskDiv.append("<br/><br/><button class='btnEdit btn btn-success'> Edit </button>");
+                    taskDiv.append("  <button class='btnDelete btn btn-danger'> Delete </button>");
+                }
+                $("#divTasks").append(taskDiv);
+
+                //Add functions to edit and delete
+                $(".btnEdit").click(function(){
+                    
+                });
+
+                (".btnDelete").click(function(){
+
+                });
+            });
+        },
+        error : function(request,error)
+        {
+            console.log(error);
+        }
+    });
+}
